ce103c177ab064a21b3d230e1a71784a75caca7b
Roxanne Nasraty <rsnasraty@gmail.com>
2020-05-05 13:32:56 -0500

    Finished Lightning Exercises

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ Lightning Exercise 1: Write a factory function that creates an object that repre
 
 
 //First create Factory Function so you can create the object that will represent your data
-const createDoctor =  (name, specialty, address) => ({
-    "doctorName": name,
-    "doctorSpecialty": specialty, 
-    "practiceAddress": address
-})
+const createDoctor = (name, specialty, address) => {
+    return {
+        "doctorName": name,
+        "doctorSpecialty": specialty,
+        "practiceAddress": address
+    }
+}
 
-//now create Doctor using the factory function so you can attach your own data 
-const doctor = createDoctor("Doctor Mario", "Magic", "Mushroom Kingdom")
-console.log(doctor);
+//now create a Doctor using the factory function so you can attach your own data 
+const doctorMario = createDoctor("Doctor Mario", "Magic", "Mushroom Kingdom")
+console.log("Doctor Mario", doctorMario);
 
 
 /* Lightning Exercise 2: Write a factory function that creates an object that represents a pet. The function should accept two arguments.
@@ -20,11 +22,18 @@ Pet name
 Pet breed
 Invoke the factory function 3 times and place each animal in an array stored in a variable named BowWowKennels */
 
+const createPet = (name, breed) => {
+    return {
+        "petName": name,
+        "petBreed": breed
+    }
+}
 
+const BowWowKennels = [
+    createPet("Melly", "Ragdoll"),
+    createPet("Cocoa", "Siamese"),
+    createPet("Luna", "Maine Coon")
 
-const createPet = ("petName", "petBreed") => ({
-"name": petName,
-"breed": petBreed
+]
 
-
-})
+console.log("BowWowKennels", BowWowKennels);
